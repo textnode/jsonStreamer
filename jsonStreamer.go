6ade8e9ee6d7dcf618c878d9a4a5babd131e16ab
textnode <darren@textnode.com>
2012-08-20 20:57:49 +0100

    String values need escaped

--- jsonStreamer.go
+++ jsonStreamer.go
@@ -16,6 +16,7 @@ package jsonStreamer
 
 import (
 	"io"
+	"strings"
 )
 
 // Ideally these would be consts, but I can't define them as consts when using chars for readability
@@ -115,6 +116,9 @@ func (self *JsonStreamer) WriteStringValue(value string) (err error) {
 	if nil != err {
 		return
 	}
+
+	value = self.escapeStringValue(value)
+
 	_, err = self.writer.Write([]byte(value))
 	if nil != err {
 		return
@@ -123,12 +127,16 @@ func (self *JsonStreamer) WriteStringValue(value string) (err error) {
 	return
 }
 
+/*
 func (self *JsonStreamer) WriteStringValueBytes(value []byte) (err error) {
 	self.lastWasKey = false
 	_, err = self.writer.Write(DOUBLE_QUOTE)
 	if nil != err {
 		return
 	}
+
+	value = self.escapeStringValueBytes(value)
+
 	_, err = self.writer.Write(value)
 	if nil != err {
 		return
@@ -136,3 +144,39 @@ func (self *JsonStreamer) WriteStringValueBytes(value []byte) (err error) {
 	_, err = self.writer.Write(DOUBLE_QUOTE)
 	return
 }
+*/
+
+func (self *JsonStreamer) escapeStringValue(value string) (result string) {
+	result = value
+	result = strings.Replace(result, "<", "\u003c", -1)
+	result = strings.Replace(result, ">", "\u003e", -1)
+	result = strings.Replace(result, "\b", "\\\b", -1)
+	result = strings.Replace(result, "\f", "\\\f", -1)
+	result = strings.Replace(result, "\n", "\\\n", -1)
+	result = strings.Replace(result, "\r", "\\\r", -1)
+	result = strings.Replace(result, "\t", "\\\t", -1)
+	result = strings.Replace(result, "\v", "\\\v", -1)
+	result = strings.Replace(result, "'", "\\'", -1)
+	result = strings.Replace(result, "\"", "\\\"", -1)
+	result = strings.Replace(result, "\\", "\\\\", -1)
+	return
+}
+
+/*
+func (self *JsonStreamer) escapeStringValueBytes(value []byte) (result []byte) {
+    value = bytes.Replace(value, []byte{"<"}, []byte{"\u003c"}, -1)
+    value = bytes.Replace(value, []byte{">"}, []byte{"\u003e"}, -1)
+	value = bytes.Replace(value, []byte{"\b"}, []byte{"\\b"}, -1)
+	value = bytes.Replace(value, []byte{"\f"}, []byte{"\\f"}, -1)
+	value = bytes.Replace(value, []byte{"\n"}, []byte{"\\n"}, -1)
+	value = bytes.Replace(value, []byte{"\r"}, []byte{"\\r"}, -1)
+	value = bytes.Replace(value, []byte{"\t"}, []byte{"\\t"}, -1)
+	value = bytes.Replace(value, []byte{"\v"}, []byte{"\\v"}, -1)
+	value = bytes.Replace(value, []byte{"\'"},  []byte{"\\'"}, -1)
+	value = bytes.Replace(value, []byte{"\'"}, []byte{"\\\""}, -1)
+	value = bytes.Replace(value, []byte{"\\"}, []byte{"\\\\"}, -1)
+	return
+}
+*/
+
+
